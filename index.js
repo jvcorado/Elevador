84443f16081e43e4f74cb1e1081d801e3514581b
João Victor Corado <jhonisilva545@gmail.com>
2022-10-11 12:04:58 -0300

    att 0.1
    animação dos elevadores

--- index.js
+++ index.js
@@ -6,14 +6,30 @@ const btnTerreo = document.querySelectorAll('#btn-terreo');
 const btnPrimeiro = document.querySelectorAll('#btn-primeiro');
 const btnSegundo = document.querySelectorAll('#btn-segundo');
 const btnTerceiro = document.querySelectorAll('#btn-terceiro');
-
+let andar = null;
 
 
 function descerTerreoAndar(){
-    document.querySelector('.img-terreo').src="img/elevadorAberto.jpg"
-    document.querySelector('.img-terceiroAndar').src="img/elevadorFechado.jpg";
-    document.querySelector('.img-segundoAndar').src="img/elevadorFechado.jpg";
-    document.querySelector('.img-primeiroAndar').src="img/elevadorFechado.jpg";
+
+    andar = 0;
+    alert('Andar ' + andar);
+
+    setTimeout(()=>{
+        document.querySelector('.img-terceirAndar').src="img/elevadorSemiAberto.jpg";
+    },1000);
+    setTimeout(()=>{
+        document.querySelector('.img-terceiroAndar').src="img/elevadorFechado.jpg";
+    },2000);
+
+
+    setTimeout(()=>{
+        document.querySelector('.img-terreo').src="img/elevadorSemiAberto.jpg";
+    },3000);
+    setTimeout(()=>{
+        document.querySelector('.img-terreo').src="img/elevadorAberto.jpg";
+    },4000);
+
+
     document.querySelector('#btn-primeiro').disabled= false;
     document.querySelector('#btn-segundo').disabled= false;
     document.querySelector('#btn-terreo').disabled= true;
@@ -21,10 +37,24 @@ function descerTerreoAndar(){
 }
 
 function subirPrimeiroAndar(){
-    document.querySelector('.img-primeiroAndar').src="img/elevadorAberto.jpg";
-    document.querySelector('.img-terreo').src="img/elevadorFechado.jpg"
-    document.querySelector('.img-segundoAndar').src="img/elevadorFechado.jpg";
-    document.querySelector('.img-terceiroAndar').src="img/elevadorFechado.jpg";
+    andar = 1;
+    alert('Andar ' + andar);
+
+
+    setTimeout(()=>{
+        document.querySelector('.img-terreo').src="img/elevadorSemiAberto.jpg";
+    },1000);
+    setTimeout(()=>{
+        document.querySelector('.img-terreo').src="img/elevadorFechado.jpg";
+    },2000);
+    setTimeout(()=>{
+        document.querySelector('.img-primeiroAndar').src="img/elevadorSemiAberto.jpg";
+    },3000);
+    setTimeout(()=>{
+        document.querySelector('.img-primeiroAndar').src="img/elevadorAberto.jpg";
+    },4000);
+   
+
     document.querySelector('#btn-terreo').disabled= false;
     document.querySelector('#btn-primeiro').disabled= true;
 }
@@ -32,10 +62,24 @@ function subirPrimeiroAndar(){
 
 
 function subirSegundoAndar(){
-    document.querySelector('.img-segundoAndar').src="img/elevadorAberto.jpg";
-    document.querySelector('.img-terreo').src="img/elevadorFechado.jpg"
-    document.querySelector('.img-primeiroAndar').src="img/elevadorFechado.jpg";
-    document.querySelector('.img-terceiroAndar').src="img/elevadorFechado.jpg";
+    andar = 2;
+    alert('Andar ' + andar);
+
+    setTimeout(()=>{
+        document.querySelector('.img-primeiroAndar').src="img/elevadorSemiAberto.jpg";
+    },1000);
+    setTimeout(()=>{
+        document.querySelector('.img-primeiroAndar').src="img/elevadorFechado.jpg";
+    },2000);
+
+
+    setTimeout(()=>{
+        document.querySelector('.img-segundoAndar').src="img/elevadorSemiAberto.jpg";
+    },3000);
+    setTimeout(()=>{
+        document.querySelector('.img-segundoAndar').src="img/elevadorAberto.jpg";
+    },4000);
+   
     document.querySelector('#btn-terreo').disabled= false;
     document.querySelector('#btn-primeiro').disabled= true;
     document.querySelector('#btn-segundo').disabled= true;
@@ -43,10 +87,24 @@ function subirSegundoAndar(){
 }
 
 function subirTerceiroAndar(){
-    document.querySelector('.img-terceiroAndar').src="img/elevadorAberto.jpg";
-    document.querySelector('.img-terreo').src="img/elevadorFechado.jpg"
-    document.querySelector('.img-primeiroAndar').src="img/elevadorFechado.jpg";
-    document.querySelector('.img-segundoAndar').src="img/elevadorFechado.jpg";
+    andar = 3;
+    alert('Andar ' + andar);
+   
+    setTimeout(()=>{
+        document.querySelector('.img-segundoAndar').src="img/elevadorSemiAberto.jpg";
+    },1000);
+    setTimeout(()=>{
+        document.querySelector('.img-segundoAndar').src="img/elevadorFechado.jpg";
+    },2000);
+
+
+    setTimeout(()=>{
+        document.querySelector('.img-terceiroAndar').src="img/elevadorSemiAberto.jpg";
+    },3000);
+    setTimeout(()=>{
+        document.querySelector('.img-terceiroAndar').src="img/elevadorAberto.jpg";
+    },4000);
+
     document.querySelector('#btn-terreo').disabled= false;
     document.querySelector('#btn-terceiro').disabled= true;
     document.querySelector('#btn-primeiro').disabled= true;
